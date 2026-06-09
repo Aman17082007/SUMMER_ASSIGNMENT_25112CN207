17d57b82ff2cd783d8eddeccf0ed2f54aebe2566
Aman17082007 <amanbabuaan3@gmail.com>
2026-06-09 07:14:07 +0530

    DAY-9 uploaded.

---
+++ day9/ques34.c
@@ -0,0 +1,18 @@
+// program to print a reverse star pattern
+#include <stdio.h>
+int main()
+{
+    int rows;
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+
+    for (int i = rows; i >= 1; i--)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
---
+++ day9/ques35.c
@@ -0,0 +1,18 @@
+// program to print repeated character pattern
+#include <stdio.h>
+
+int main()
+{
+    int i, j;
+
+    for (i = 0; i < 5; i++)
+    {
+        for (j = 0; j <= i; j++)
+        {
+            printf("%c", 'A' + i);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
