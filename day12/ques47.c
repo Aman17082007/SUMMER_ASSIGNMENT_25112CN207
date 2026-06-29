d6a2bc89a1bd289c079ef67f4ba94afc63d07a56
Aman17082007 <amanbabuaan3@gmail.com>
2026-06-29 07:26:32 +0530

    DAY-12 uploaded.

---
+++ day12/ques47.c
@@ -0,0 +1,31 @@
+// program to write function for fibonacci
+#include <stdio.h>
+
+// Function to print Fibonacci series
+void fibonacci(int n)
+{
+    int first = 0, second = 1, next, i;
+
+    printf("Fibonacci Series: ");
+
+    for (i = 1; i <= n; i++)
+    {
+        printf("%d ", first);
+
+        next = first + second;
+        first = second;
+        second = next;
+    }
+}
+
+int main()
+{
+    int terms;
+
+    printf("Enter number of terms: ");
+    scanf("%d", &terms);
+
+    fibonacci(terms);
+
+    return 0;
+}
