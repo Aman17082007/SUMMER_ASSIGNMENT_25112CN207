1ff25f4c4473b06ae733bafcd81718d83f7aeee3
Aman17082007 <amanbabuaan3@gmail.com>
2026-06-30 07:07:53 +0530

    DAY-20 uploaded.

---
+++ day20/ques78.c
@@ -0,0 +1,42 @@
+// program to check symmetric matrix
+#include <stdio.h>
+
+int main()
+{
+    int n, i, j;
+    int a[10][10], symmetric = 1;
+
+    printf("Enter the order of the square matrix: ");
+    scanf("%d", &n);
+
+    printf("Enter the elements of the matrix:\n");
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    // Check for symmetry
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            if (a[i][j] != a[j][i])
+            {
+                symmetric = 0;
+                break;
+            }
+        }
+        if (symmetric == 0)
+            break;
+    }
+
+    if (symmetric)
+        printf("The matrix is symmetric.\n");
+    else
+        printf("The matrix is not symmetric.\n");
+
+    return 0;
+}
